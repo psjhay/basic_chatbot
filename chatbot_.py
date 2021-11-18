aea84a95cf59fa2882615dd014b583a89fb74732
psjhay <joyceamymamah@gmail.com>
2021-11-18 16:28:57 +0100

    Second version of the basic chatbot

--- chatbot_.py
+++ chatbot_.py
@@ -1,3 +1,10 @@
+'''
+A list of dictionaries. it contains words that serve as reference. A user input
+must contain all the words in a given key to successfully map it to a value.
+More words can be added when giving an input; the bot will still give a right
+answer provided that all the words in the key are in your input.
+'''
+
 chatbot = [ {"user_input":"hello", "bot_resp":"Hello!"},
   {"user_input":"how are", "bot_resp":"I'm doing good. You?"},
   {"user_input":"fine", "bot_resp":"That's good"},
@@ -10,6 +17,13 @@ chatbot = [ {"user_input":"hello", "bot_resp":"Hello!"},
   {"user_input":"thanks","bot_resp":"You're Welcome!"},
   {"user_input":"bye", "bot_resp":"Goodbye! Hope to see you some other time"} ]
 
+
+#chatbot response system
+'''
+this is an infinite loop that first requires an input from a user.
+it then checks the input and prints a certain response based on that input.
+the loop then continues to run until it is interrupted again by a user input.
+'''
 while True:
     user_input = input("You: ")
     loc_answer = False
